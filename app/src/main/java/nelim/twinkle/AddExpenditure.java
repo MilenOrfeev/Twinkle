56e465f0323598e822299da1e7d12344d779ed49
bouncypenguin <milen.orfeev@abv.bg>
2018-02-15 23:35:06 +0000

    Fixed bugs, purchases are added in a well documented way.

--- app/src/main/java/nelim/twinkle/AddExpenditure.java
+++ app/src/main/java/nelim/twinkle/AddExpenditure.java
@@ -2,7 +2,6 @@ package nelim.twinkle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,37 +11,40 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 
-public class AddExpenditure extends AppCompatActivity {
+// This class takes care of adding new expenditures to the database.
+public class AddExpenditure extends AppCompatActivity
+{
 
+    // Get a reference from the database.
     FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     DatabaseReference mDatabaseReference= mDatabase.getReference();
 
-    // Add new users to the database.
+    // Get the current user.
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenditure);
-    }
+    } // onCreate
 
     // Called when user taps add button
     public void buttonAdd(View view)
     {
 
+        // Add the user only if they have logged in.
         if(user != null)
             addUser(user);
 
-        // Create a product object.
+        // Create an expenditure object.
         EditText purchaseTextBox = findViewById(R.id.purchaseTextBox);
         EditText txtCost = findViewById(R.id.costTextBox);
         double cost = Double.parseDouble(txtCost.getText().toString());
@@ -51,44 +53,44 @@ public class AddExpenditure extends AppCompatActivity {
         expenditure = new Expenditure(purchaseTextBox.getText().toString(),cost, dateOfPurchase);
         Map<String, Object> mappedExpenditure = expenditure.toMap();
 
-        addProduct(mappedExpenditure);
+        // Add it to the database.
+        countAndAddExpenditures(mappedExpenditure);
 
     }
 
-    private void addProduct(final Map<String, Object> mappedExpenditure)
+    // Need the number of expenditures in order to add the new one
+    private void countAndAddExpenditures(final Map<String, Object> mappedExpenditure)
     {
-        Query lastQuery =
-                mDatabaseReference.child("users").child(user.getUid())
-                        .orderByChild("Product number").limitToLast(1);
-
-        lastQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+        // Listener is for single value event since data is needed only once.
+        mDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot)
             {
-                //String productNo = dataSnapshot.child("users").child(user.getUid())
-                   //     .child("Product number").getValue().toString();
-                long noOfProducts = dataSnapshot.child("users").child(user.getUid())
+                // Get the count of expenditures for that particular user.
+                long noOfExpenditures = dataSnapshot.child("users").child(user.getUid())
                         .getChildrenCount();
-                addProductNo(noOfProducts, mappedExpenditure);
+                addExpenditure(noOfExpenditures, mappedExpenditure);
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-                //Handle possible errors.
+            public void onCancelled(DatabaseError databaseError)
+            {
+                System.out.println("rejected");
             }
         });
     }
 
-    private void addProductNo(long noOfProducts, Map<String, Object> mappedExpenditure)
+    // Invoked after all the necessary data is collected.
+    private void addExpenditure(long noOfExpenditures, Map<String, Object> mappedExpenditure)
     {
 
-        noOfProducts +=1;
-        String productNo = noOfProducts + "";
-        Map<String, Object> productToProductNo = new HashMap<>();
-        productToProductNo.put("Product number", productNo);
-        mDatabaseReference.child("users").child(user.getUid())
-                .updateChildren(productToProductNo);
-        mDatabaseReference.child("users").child(user.getUid()).child(productNo)
+        // Needed in a string format in order to be added into the database.
+        long newExpenditureNo = noOfExpenditures + 1;
+        String expenditureNo = "Expenditure " + newExpenditureNo;
+
+        // The only information about the user is about their purchases, so it is no
+        // problem to be added right underneath the userID.
+        mDatabaseReference.child("users").child(user.getUid()).child(expenditureNo)
                 .updateChildren(mappedExpenditure);
 
     }
@@ -104,23 +106,21 @@ public class AddExpenditure extends AppCompatActivity {
             {
                 if (!snapshot.hasChild(user.getUid()))
                 {
-                    System.out.println(!snapshot.hasChild(user.getUid()));
                     writeNewUser(user);
-                }
-            }
-
+                } // if
+            } // onDataChange
 
             @Override
             public void onCancelled(DatabaseError databaseError) {}
-        });
-    }
+        }); // addListerForSingleValueEvent
+    } // addUser
 
     // Include the user in the database.
     private void writeNewUser(FirebaseUser user)
     {
         mDatabaseReference.child("users").child(user.getUid()).setValue(user.getDisplayName());
-    }
+    } // writeNewUser
 
-}
+} // addExpenditure
 
 
